a5095f62d5b81694780aabb7167a0da3f73ad51c
Ritam333 <ritambera2002@gmail.com>
2025-04-22 22:21:17 +0530

    hi

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("ATS Resume Screening System")
 
-option = st.selectbox("Select a Dashboard", ["-- Select --", "HR Dashboard", "Job Seeker Dashboard"])
+# option = st.selectbox("Select a Dashboard", ["-- Select --", "HR Dashboard", "Job Seeker Dashboard"])
 
 if option == "HR Dashboard":
     st.switch_page("pages/1️⃣ HR Dashboard.py")
